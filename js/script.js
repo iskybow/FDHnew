96b3d721bdf5f6cf3bf23321d57822b414f56a2f
@boyko <kir_boyko@bk.ru>
2019-11-28 17:41:07 +0300

    sections

--- js/script.js
+++ js/script.js
@@ -2,4 +2,10 @@ $(document).ready(function () {
     $('.jsMobileMenu').click(function () {
         $('.jsMenu').toggleClass('menu-show');
     });
+
+    $("#calc_slider1").ionRangeSlider({
+        min: 0,
+        max: 6000000,
+        step: 10000
+    });
 });
